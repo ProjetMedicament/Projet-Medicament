2dbe322b9b2104835a3a28acf80140e09d57f8f8
Younness SAID MOHAMED <younnessai@gmail.com>
2021-11-12 11:49:24 +0100

    mo

--- ProjetWPF/MainWindow.xaml.cs
+++ ProjetWPF/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace ProjetWPF
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();  llll
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
