89e5548bb56ff498b5267a38e1cf5e737966d89c
Younness SAID MOHAMED <younnessai@gmail.com>
2021-12-09 09:55:19 +0100

    Tous les XAML sauf statistiques

---
+++ ProjetWPF/Vues/Visiteurs.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjetWPF.Vues
+{
+    /// <summary>
+    /// Logique d'interaction pour Visiteurs.xaml
+    /// </summary>
+    public partial class Visiteurs : Window
+    {
+        public Visiteurs()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void lstVisiteurs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnValider_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRetour_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
